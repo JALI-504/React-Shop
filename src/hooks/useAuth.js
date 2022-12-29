e18a810a935fb9787f5d4816d9c9b67ce62d6431
JALI <furio504@hotmail.com>
2022-12-29 15:11:04 -0600

    COrreccion en autentificacion

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,7 @@
 import React, { useState, useContext, createContext } from 'react';
-import Cookies from 'js-cookie';
+import Cookie from 'js-cookie';
 import axios from 'axios';
 import endPoints from '@services/api/';
-
 
 const authContext = createContext();
 
@@ -19,16 +18,24 @@ function useProvideAuth() {
   const [user, setUser] = useState(null);
 
   const signIn = async (email, password) => {
-    //setUser('login');
     const options = {
-        Headers: {
-            accept: '*/*',
-            'Content-Type': 'application/json',
-        },
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
     };
-    const { data:acces_token } = await axios.post(endPoints.auth.login, {email, password}, options);
-    console.log(acces_token);
+    const { data: access_token } = await axios.post(endPoints.auth.login, { email, password }, options);
+    if(access_token) {
+      const token = access_token.access_token; 
+      Cookie.set('token', token, {expires: 5});
+     // console.log(access_token);
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      console.log(user);
+      setUser(user);
+    }
   };
+
   return {
     user,
     signIn,
